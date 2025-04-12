36a4b0b4f0335625f6609a1d5543d4edb7db8086
J-uds <206530139+J-uds@users.noreply.github.com>
2025-04-12 20:09:34 +0200

    done first 10 exercises

---
+++ scripts.js
@@ -0,0 +1,49 @@
+//1. Escribe una función que tome dos números como argumentos y devuelva su suma.
+function sum(num1, num2){
+    return num1+num2;
+}
+
+//2. Escribe una función que tome dos números como argumentos y devuelva su resta.
+function sustraction(num1, num2){
+    return num1-num2;
+}
+
+//3. Escribe una función que tome dos números como argumentos y devuelva su producto.
+function product(num1, num2){
+    return num1*num2;
+}
+
+//4. Escribe una función que tome dos números como argumentos y devuelva su división.
+function fraction(num1, num2){
+    return num1/num2;
+}
+
+//5. Escribe una función que tome un número base y un exponente y devuelva el resultado de elevar el número base a la potencia dada.
+function power(num1, num2){
+    return num1**num2;
+}
+
+//6. Escribe una función que tome dos números como argumentos y devuelva el resto de la división del primer número entre el segundo.
+function restFraction(num1, num2){
+    return num1%num2;
+}
+
+//7. Escribe una función que tome un número como argumento y devuelva su raíz cuadrada.
+function squared(num1){
+    return Math.sqrt(num1);
+}
+
+//8. Escribe una función que tome un número como argumento y devuelva su valor absoluto.
+function absolute(num1){
+    return Math.abs(num1);
+}
+
+//9. Escribe una función que tome un número decimal como argumento y devuelva el número redondeado al entero más cercano.
+function rounded(num1){
+    return Math.round(num1);
+}
+
+//10. Escribe una función que genere y devuelva un número aleatorio entre 0 y 1.
+function randomly(){
+    return Math.random();
+}
