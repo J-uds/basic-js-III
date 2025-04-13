0b570c779ee16a1af32fd8f5863883a6424f73bf
J-uds <206530139+J-uds@users.noreply.github.com>
2025-04-13 12:35:10 +0200

    done exercise 1 third part

--- scripts.js
+++ scripts.js
@@ -112,4 +112,24 @@ function firstLetter(string1) {
     }
     return eachWord.join(" ");
 }
-console.log(firstLetter(string1));
+
+// 1. Escribe una función que tome un array de números como argumento y devuelva la suma de todos sus elementos.
+let numbers = [1, 2, 3, 4, 5]
+function sumArray (numbers) {
+    let sum = 0;
+    for (i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+// 2. Escribe una función que tome un array de números como argumento y devuelva el promedio de todos sus elementos.
+
+// 3. Escribe una función que tome un array de números como argumento y devuelva el array ordenado de forma ascendente.
+// 4. Escribe una función que tome un array de números y un número como argumentos, y devuelva un nuevo array con los elementos mayores al número dado.
+// 5. Escribe una función que tome dos arrays como argumentos y devuelva un nuevo array con todos los elementos de ambos arrays.
+// 6. Escribe una función que tome un array de números como argumento y devuelva el número máximo dentro del array.
+// 7. Escribe una función que tome un array de números como argumento y devuelva el número mínimo dentro del array.
+// 8. Escribe una función que tome un array y un elemento como argumentos, y devuelva la cantidad de veces que el elemento aparece en el array.
+// 9. Escribe una función que tome un array como argumento y devuelva un nuevo array sin elementos duplicados.
+// 10. Escribe una función que tome un array como argumento y devuelva un nuevo array con los elementos en orden inverso.
